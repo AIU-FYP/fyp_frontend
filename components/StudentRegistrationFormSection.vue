52d20bddcc53955c2f3de81e94242a03e1f564ba
AdnanMadi417 <164775717+AdnanMadi417@users.noreply.github.com>
2024-11-16 17:15:55 +0800

    remove Number from label of register new student form and fixed container issues

--- components/StudentRegistrationFormSection.vue
+++ components/StudentRegistrationFormSection.vue
@@ -1,7 +1,60 @@
 <script setup lang="ts">
-import { ref} from 'vue'
+
+import {onMounted, ref} from 'vue'
+import {useNuxtApp} from '#app'
+import Popup from '~/components/PopupStudentMaintenanceRoom.vue'
+
+interface Person {
+  id: number
+  date: string
+  name: string
+  studentIdNumber: string
+  roomNumber: string
+  whatsappNumber: string
+  emailAddress: string
+  gender: string
+  extend?: boolean | string
+}
+
+const columns = [
+  {key: 'id', label: 'id'},
+  {key: "date", label: 'Date',},
+  {key: 'name', label: 'Name', sortable: true},
+  {key: 'studentId', label: 'Student ID', sortable: true},
+  {key: 'roomNo', label: 'Room No', sortable: true},
+  {key: 'whatsappNo', label: 'WhatsApp No', sortable: true},
+  {key: 'gender', label: 'Gender', sortable: true},
+  {key: 'status', label: 'Status', sortable: true},
+  {key: 'extend', label: 'Extend', sortable: false,}
+]
+
+const people = ref<Person[]>([]);
+
+let {$axios} = useNuxtApp()
+const api = $axios()
+
+const fetchData = async () => {
+  try {
+    const response = await api.get("/Students/")
+    people.value = response.data.map((person: Person) => ({
+      ...person,
+      date: new Date().toLocaleDateString()
+    }))
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
 
 
+const isPopupVisible = ref(false);
+const currentStudent = ref({});
+
+const openPopup = (row: Person) => {
+  currentStudent.value = row;
+  isPopupVisible.value = true;
+};
+
+onMounted(fetchData)
 
 
 const visibleButtonIndex = ref<number | null>(null);
@@ -49,43 +102,62 @@ function toggleLinkVisibility(index: number) {
 <template>
 
   <div class="admin-dashboard">
+    <div class="container">
 
-    <aside class="sidebar">
-      <div v-for="(button, index) in navigationButtons" :key="index">
-        <div class="btn-container">
-          <button
-              @click="toggleLinkVisibility(index)"
-              :aria-expanded="visibleButtonIndex === index"
-              class="sidebar-button"
-          >
-            <UIcon
-                :name="button.icon"
-            />
-            {{ button.name }}
-          </button>
+      <aside class="sidebar">
+        <div v-for="(button, index) in navigationButtons" :key="index">
+          <div class="btn-container">
+            <button
+                @click="toggleLinkVisibility(index)"
+                :aria-expanded="visibleButtonIndex === index"
+                class="sidebar-button"
+            >
+              <UIcon
+                  :name="button.icon"
+              />
+              {{ button.name }}
+            </button>
+          </div>
+          <ul v-if="visibleButtonIndex === index">
+            <li v-for="(link, linkIndex) in button.links" :key="linkIndex">
+              <a :href="link.url">{{ link.text }}</a>
+            </li>
+          </ul>
         </div>
-        <ul v-if="visibleButtonIndex === index">
-          <li v-for="(link, linkIndex) in button.links" :key="linkIndex">
-            <a :href="link.url">{{ link.text }}</a>
-          </li>
-        </ul>
-      </div>
-    </aside>
+      </aside>
 
-    <main class="dashboard-content">
-      <StudentRegistrationForm/>
-    </main>
+      <main class="dashboard-content">
+        <StudentRegistrationForm/>
+
+      </main>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
 .admin-dashboard {
-  display: flex;
+  display: block;
 }
 
+.admin-dashboard > .container {
+  display: flex;
+  flex-wrap: nowrap;
+  padding: 0;
+  margin: 0 auto;
+  max-width: 1300px;
+  width: 100%;
+  border: 3px solid var(--main-color);
+  border-radius: 0 1rem 1rem 0;
+}
+
+@media (max-width: 1200px) {
+  .admin-dashboard .container{
+    border-radius: 0;
+    border: none;
+  }
+}
 .sidebar {
-  flex: 1;
+  flex: 0 0 15%;
   background-color: var(--main-color);
   padding: 2rem 1rem;
   display: flex;
@@ -93,23 +165,8 @@ function toggleLinkVisibility(index: number) {
   min-height: 100vh;
 }
 
-@media (max-width: 1200px) {
-  .admin-dashboard {
-    display: block;
-  }
-
-  .sidebar {
-    min-height: 30vh;
-  }
-}
-
-.btn-container {
-  padding: .5rem;
-  background-color: transparent;
-}
-
-.btn-container:hover {
-  background-color: var(--main-hovor-color);
+.dashboard-content {
+  flex: 1;
 }
 
 .sidebar-button {
@@ -117,49 +174,78 @@ function toggleLinkVisibility(index: number) {
   color: var(--text-color);
   margin-bottom: 0.5rem;
   text-align: start;
-  border-radius: .5rem;
-  transition: 0.3s ease-in-out;
+  border-radius: 0.5rem;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
 }
 
 .sidebar-button:hover {
   color: var(--text-hovor-color);
+  background-color: var(--main-hovor-color);
+  transition: 0.3s ease-in-out;
+}
+
+.btn-container {
+  display: flex;
+  margin-bottom: .5rem;
+  font-size: 1rem;
+  font-weight: normal;
+  color: var(--text-color);
+  background-color: transparent;
+}
+
+.btn-container:hover {
+  display: flex;
+  margin-bottom: .5rem;
+  font-size: 1rem;
+  font-weight: normal;
+  color: var(--text-color);
+  background-color: var(--main-hovor-color);
+}
+
+
+.sidebar ul {
+  padding: 0;
+  margin: 0;
 }
 
 .sidebar ul li {
   list-style: none;
-  margin: 0.5rem;
+  margin: 0.5rem 0;
   padding: 0.5rem;
   font-size: 1rem;
   text-align: start;
   text-transform: capitalize;
   font-weight: normal;
-  color: var(--text-hovor-color);
+  color: var(--text-color);
   background-color: transparent;
 }
 
-.sidebar li:hover {
+.sidebar ul li:hover {
   color: var(--text-hovor-color);
   background-color: var(--main-hovor-color);
-  transition: .3s ease-in-out;
 }
 
-.dashboard-content {
-  flex: 5;
-  padding: 0;
-  background-color: #eeeeee;
+@media (max-width: 1200px) {
+  .admin-dashboard > .container {
+    flex-direction: column;
+  }
+
+  .sidebar {
+    min-height: 30vh;
+    flex-basis: 100%;
+  }
 }
 
 @media (max-width: 768px) {
   .sidebar {
     flex-basis: 100%;
+    min-width: 0;
   }
 
   .dashboard-content {
     padding: 1rem;
   }
-
 }
-
-
-
 </style>
